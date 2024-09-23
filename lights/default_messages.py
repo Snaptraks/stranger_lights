eb85b7e0fe5730d6609f31655e27cf73d17e5707
Snaptraks <snaptraks@gmail.com>
2024-09-23 09:17:39 -0400

    Add default messages

--- lights/default_messages.py
+++ lights/default_messages.py
@@ -1,11 +1,20 @@
 from random import choice
 
 default_messages: list[str] = [
+    "he is coming",
+    "he is here",
     "help me",
+    "i am hungry",
+    "i am not alone",
+    "i am trapped",
+    "i cant escape",
+    "i have hope",
+    "it is so lonely",
+    "it stinks",
+    "please come",
     "right here",
     "so cold",
-    "it stinks",
-    "he is here",
+    "so scared",
 ]
 
 
